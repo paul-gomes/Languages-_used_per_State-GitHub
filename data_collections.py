8cd6d34771c9778507a7e53108a300cb428921ed
Anik Paul Gomes <gomesap29@gmail.com>
2019-03-28 00:57:52 -0500

    getting users for every states

--- data_collections.py
+++ data_collections.py
@@ -40,34 +40,33 @@ def maximum_repos(location):
 
 
 """
-Goal: function that gets all the unique user for a specific state
-Function takes users_set and users as parameter and goes thorugh different 
-pages of users paginated list and adds unique named user to the user_set set
-return: user_set
+Goal: function that gets all the users for a specific state
+Function takes usrs_list and usrs as parameter and goes thorugh different 
+pages of users paginated list and adds named user to the usr_list set
+return: usr_list
     
 """
 
 
 
-def get_user_by_location(usrs_set, usrs):
+def get_user_by_location(usrs_list, usrs):
     page_number = 0
     count = 0
     try:
         while(count <= usrs.totalCount):
             time.sleep(6)
-            page = usrs.get_page(page_number)
-            if len(page) == 0: return usrs_set
-            for usr in page:
-                usrs_set.add(usr)
-            count += len(page)
+            usrs_per_page = usrs.get_page(page_number)
+            if len(usrs_per_page) == 0: return usrs_list
+            usrs_list.extend(usrs_per_page)
+            count += len(usrs_per_page)
             page_number += 1
             print("page number", page_number)
             print("count", count)
-        return usrs_set
+        return usrs_list
     
     except Exception as e:
         print(str(e))
-        return usrs_set
+        return usrs_list
     
     
     
@@ -152,60 +151,115 @@ print(maximum_repo_state)
 
 
 #pickleing the maximum_repo_state dictionary 
-with open('maximum_repo_state.pkl', 'wb') as f:
+with open('pickles/maximum_repo_state.pkl', 'wb') as f:
     pickle.dump(maximum_repo_state, f)
-'''
+    
+
 
 
 
 #Reads the maximum_repo_state pickle
-with open('maximum_repo_state.pkl', 'rb') as f:
+with open('pickles/maximum_repo_state.pkl', 'rb') as f:
     maximum_repo_state  = pickle.load(f)
+
+print(maximum_repo_state)
+
+maximum_repo_state = {'Florida': 901, 'FL': 751, 'Georgia': 501, 'GA': 551, 'Hawaii': 201, 'HI': 301, 'Idaho': 401, 'ID': 251, 'Illinois': 501, 'IL': 1551, 'Indiana': 251, 'IN': 33601, 'Iowa': 451, 'IA': 501, 'Kansas': 401, 'KS': 1201, 'Kentucky': 301, 'KY': 401, 'Louisiana': 1851, 'LA': 20301, 'Maine': 551, 'ME': 251, 'Maryland': 501, 'MD': 1101, 'Massachusetts': 1151, 'MA': 1801, 'Michigan': 551, 'MI': 3951, 'Minnesota': 1001, 'MN': 551, 'Mississippi': 351, 'MS': 201, 'Missouri': 501, 'MO': 1901, 'Montana': 251, 'MT': 401, 'Nebraska': 351, 'NE': 951, 'Nevada': 11751, 'NV': 1301, 'New Hampshire': 51, 'NH': 1051, 'New Jersey': 51, 'NJ': 1251, 'New Mexico': 51, 'NM': 1001, 'New York': 401, 'NY': 15701, 'North Carolina': 101, 'NC': 1451, 'North Dakota': 51, 'ND': 451, 'Ohio': 751, 'OH': 901, 'Oklahoma': 251, 'OK': 251, 'Oregon': 751, 'OR': 751, 'Pennsylvania': 451, 'PA': 501, 'Rhode Island': 51, 'RI': 551, 'South Carolina': 51, 'SC': 1351, 'South Dakota': 151, 'SD': 401, 'Tennessee': 551, 'TN': 801, 'Texas': 1951, 'TX': 2951, 'Utah': 551, 'UT': 1101, 'Vermont': 701, 'VT': 251, 'Virginia': 1701, 'VA': 1351, 'Washington': 801, 'WA': 2351, 'West Virginia': 51, 'WV': 501, 'Wisconsin': 351, 'WI': 851, 'Wyoming': 401, 'WY': 251}
+
+'''
+maximum_repo_state = {'Delaware': 251, 'DE': 20301}
 
 
 
 #Gets users of all locations from the maximum_repo_state
 
-
-#Writes the header of the csv file which stores the location, all the named users, number of users
-with open('github_users_by_state.csv', 'a', newline='') as csvFile:
-    writer = csv.writer(csvFile)
-    writer.writerow(["Location", "Users", "Number of users"])
-
-csvFile.close()
-
-
-
 """
 Goes thorugh all the items in the maximum_repo_state dictionary.
 Uses key as a location and value as the maximum repo to iterate through.
-It searches users with specific repos (which increases by 1, and goes until maximum repo) and for a specific location
-It uses get_user_by_location to get uniques users for that location and specific repo number, and returns user_set
-Eventually, appends the location, users, number of users into the csv.
+It searches users with specific repos (which increases by 1, and goes until maximum repo) and for a specific location(key)
+It uses get_user_by_location to get users for that location and specific repo number, and returns user_list
+At the end, it appends the location, users, number of users into the github_data_dict and pickles it
 
 """
 
 
+github_data_dict = {}
+
 for key, value in maximum_repo_state.items():
     print("key: {} & value: {}".format(key, value))
-    users_set= set()
+    users_list = []
     repos = 1
     state = key
     users =  client.search_users(query= 'repos:{} location:{}'.format(repos, state ))
     print("State {} & repos: {} & number of users {}".format(state, repos, users.totalCount))
 
     while(repos <= value ):
-        users_set = get_user_by_location(users_set, users)
-        print("length of users set", len(users_set))
+        users_list = get_user_by_location(users_list, users)
+        print("length of users_list", len(users_list))
         print("----------------------next--------------------------------")
         repos += 1
         users =  client.search_users(query= 'repos:{} location:{}'.format(repos, state ))
         print("State {} & repos: {} & number of users {}".format(state, repos, users.totalCount))
     
-    print(users_set)
-    with open('github_users_by_state.csv', 'a', newline='') as csvFile:
+    print(users_list)
+    
+    github_data_dict[key] = users_list
+    
+    
+#pickleing the maximum_repo_state dictionary 
+with open('pickles/{}.pkl'.format(key), 'wb') as f:
+    pickle.dump(github_data_dict, f)
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+
+with open('pickles/connecticut.pkl', 'rb') as f:
+    maximum_repo_state  = pickle.load(f)
+
+print(len(maximum_repo_state.get('Connecticut')))
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+#Writes the header of the csv file which stores the location, all the named users, number of users
+with open('data/github_users_by_state.csv', 'a', newline='') as csvFile:
+    writer = csv.writer(csvFile)
+    writer.writerow(["Location", "Users", "Number of users"])
+
+csvFile.close()
+
+    
+    with open('data/github_users_by_state.csv', 'a', newline='') as csvFile:
         writer = csv.writer(csvFile)
-        writer.writerow([key, users_set, len(users_set)])
+        writer.writerow([key, users_list, len(users_list)])
     csvFile.close()
+
+
+"""
 
 
